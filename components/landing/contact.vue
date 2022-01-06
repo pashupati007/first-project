a02b61b7f7cf884bc5480ada79820a18532b187c
Pashupati <chypashu@gmail.com>
2022-01-06 16:26:23 +0545

    update contactus

--- components/landing/contact.vue
+++ components/landing/contact.vue
@@ -5,7 +5,6 @@
         <v-img :src="require('~/assets/img/bottle.png')"></v-img>
       </v-col>
 
-
       <v-col cols="10" algin="center">
         <v-img
           :src="require('~/assets/img/contactback.png')"
@@ -14,10 +13,10 @@
           class="mt-n3 mb-lg-8"
         >
           <v-row class="mt-lg-10 pt-lg-5">
-            <v-col align="center">
-              <h1 class="h1blood mb-lg-n5">रागत चहियो ?</h1>
+            <v-col>
+              <h1 class="h1blood mb-lg-n5" align="center">रागत चहियो ?</h1>
               <br />
-              <p class="wrap">
+              <p class="font wrap">
                 Fill in the form and send us your details.<br />
                 Someone will get back to you if it's an <br />
                 emergency.
@@ -26,35 +25,39 @@
           </v-row>
 
           <v-row class="mt-10" justify="center">
-            <v-btn color="white" class="mr-5 pa-lg-5" outlined> Request Blood </v-btn>
+            <v-btn color="white" class="mr-5 pa-lg-5" outlined>
+              Request Blood
+            </v-btn>
 
-            <v-btn color="white" class="ml-5 pa-lg-5" outlined> Donate Blood Now</v-btn>
+            <v-btn color="white" class="ml-5 pa-lg-5" outlined>
+              Donate Blood Now</v-btn
+            >
           </v-row>
 
-          <v-row class="mt-10 ml-16 pl-16 pt-10">
+          <v-row class="mt-10 ml-16 pl-16">
             <v-col cols="3 mt-16">
-              <v-list-item class="space">
+              <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title class="font-weight-bold text-sm-left">
+                  <h1 class="font font-weight-bold text-sm-left">
                     Contact info
-                  </v-list-item-title>
-                  <v-list-item-subtitle class="font-weight-medium text-sm-left">
+                  </h1>
+                  <p class="font space font-weight-medium text-sm-left">
                     Address: Santinagar-05, <br />
                     Kathmandu,Nepal <br />
                     Phone:9825783377 <br />
                     Email: xyzcompany@gmail.com <br />
-                  </v-list-item-subtitle>
+                  </p>
                 </v-list-item-content>
               </v-list-item>
             </v-col>
 
             <v-col cols="3 mt-16">
-              <v-list-item class="space">
+              <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title class="font-weight-bold text-sm-left">
-                    Resources</v-list-item-title
-                  >
-                  <v-list-item-subtitle class="font-weight-medium text-sm-left">
+                  <h1 class="font font-weight-bold text-sm-left">
+                    Resources
+                  </h1>
+                  <p class="space font font-weight-medium text-sm-left">
                     Blood basics <br />
                     Blood bank information <br />
                     When you need blood <br />
@@ -62,23 +65,23 @@
                     Reports <br />
                     Videos <br />
                     Media Kit <br />
-                  </v-list-item-subtitle>
+                  </p>
                 </v-list-item-content>
               </v-list-item>
             </v-col>
 
             <v-col cols="3 mt-16">
-              <v-list-item class="space">
+              <v-list-item>
                 <v-list-item-content>
-                  <v-list-item-title class="font-weight-bold text-sm-left">
+                  <h1 class="font font-weight-bold text-sm-left">
                     Newsletter
-                  </v-list-item-title>
-                  <v-list-item-subtitle class="font-weight-medium text-sm-left">
+                  </h1>
+                  <p class="space font font-weight-medium text-sm-left">
                     Subscribe to our <br />
                     Newsletter and get the <br />
                     latest updates about<br />
                     Blood Donation. <br />
-                  </v-list-item-subtitle>
+                  </p>
                 </v-list-item-content>
               </v-list-item>
             </v-col>
@@ -96,10 +99,9 @@
       </v-col>
     </v-row>
 
-
     <v-footer class="background mt-lg-16">
       <hr />
-      <p class="pl-10 mb-0">
+      <p class="font pl-10 mb-0">
         Copyright 2022 Donation Group of Companies | All rights reserved |
         Privacy Policy
       </p>
@@ -112,12 +114,17 @@
   background-color: white !important;
 }
 .space {
-  line-height: 10px !important;
+  line-height: 20px !important;
 }
 .h1blood {
   color: white !important;
+  font-size: 40px !important;
 }
 .wrap {
   white-space: wrap;
+  margin-left: 480px;
+}
+.font {
+  font-family: Poppins;
 }
 </style>
