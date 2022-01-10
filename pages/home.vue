b538a1a0f4596a6804955f70b3f30c99444da1b4
Pashupati <chypashu@gmail.com>
2022-01-10 14:05:01 +0545

    update

--- pages/home.vue
+++ pages/home.vue
@@ -1,25 +1,15 @@
 <template>
   <div>
 
-    <div>
-      <UserTool />
-    </div>
-
-    <div>
-      <UserNavdrawer />
-    </div>
     <v-container>
       <v-row>
-        <v-col cols="12" lg="8">
-
-        </v-col>
+        <v-col cols="12" lg="8"> </v-col>
 
         <v-col cols="12" lg="4">
           <div>
-      <UserDonarsearch />
-    </div>
+            <UserDonarsearch />
+          </div>
         </v-col>
-
       </v-row>
     </v-container>
   </div>
