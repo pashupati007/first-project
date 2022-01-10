eeef39367a523495857892b315df5ee5081edf70
Pashupati <chypashu@gmail.com>
2022-01-10 14:38:20 +0545

    update newdonor

--- components/user/newdonor.vue
+++ components/user/newdonor.vue
@@ -1,7 +1,16 @@
 <template>
     <div>
         <v-card width="410" height="580">
-
+            <h2> New Donors</h2>
+            <v-list>
+                <v-list-item>
+                    <v-avatar><img :src="require('~/assets/img/blog3.png')" alt=""></v-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>Erica Lyanda <v-icon>mdi-shield-cross-outline</v-icon> </v-list-item-title>
+                        <v-list-item-subtitle> <v-icon></v-icon>France</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-card>
     </div>
 </template>
