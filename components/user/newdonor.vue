05a90667cd2a5f0b55f6383d1b0540b81255fbdb
Pashupati <chypashu@gmail.com>
2022-01-10 15:22:11 +0545

    update

--- components/user/newdonor.vue
+++ components/user/newdonor.vue
@@ -1,13 +1,37 @@
 <template>
     <div>
-        <v-card width="410" height="580">
-            <h2> New Donors</h2>
-            <v-list>
+        <v-card  class="ml-10 mt-10 mb-16" width="410" height="350">
+            <h2 class="text-center pt-5"> New Donors</h2>
+            <v-list class="ml-10">
                 <v-list-item>
                     <v-avatar><img :src="require('~/assets/img/blog3.png')" alt=""></v-avatar>
                     <v-list-item-content>
-                        <v-list-item-title>Erica Lyanda <v-icon>mdi-shield-cross-outline</v-icon> </v-list-item-title>
+                        <v-list-item-title>Erica Lyanda <v-icon>mdi-message-reply</v-icon> </v-list-item-title>
                         <v-list-item-subtitle> <v-icon></v-icon>France</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-avatar><img :src="require('~/assets/img/blog3.png')" alt=""></v-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>Erica Lyanda <v-icon>mdi-message-reply</v-icon> </v-list-item-title>
+                        <v-list-item-subtitle> <v-icon></v-icon>England</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-avatar><img :src="require('~/assets/img/blog3.png')" alt=""></v-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>Erica Lyanda <v-icon>mdi-message-reply</v-icon> </v-list-item-title>
+                        <v-list-item-subtitle> <v-icon></v-icon>China</v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                    <v-avatar><img :src="require('~/assets/img/blog3.png')" alt=""></v-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>Erica Lyanda <v-icon>mdi-message-reply</v-icon> </v-list-item-title>
+                        <v-list-item-subtitle> <v-icon></v-icon>India</v-list-item-subtitle>
                     </v-list-item-content>
                 </v-list-item>
             </v-list>
