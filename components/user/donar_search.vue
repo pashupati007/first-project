cb005963c8da5ee56fe5f771ff29fa7f900c94a9
Pashupati <chypashu@gmail.com>
2022-01-10 13:24:42 +0545

    update

---
+++ components/user/donar_search.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+    <v-card
+    width="450"
+    height="550">
+
+    <v-list>
+        <v-list-item>
+
+            <v-list-content>
+                <v-list-item-title>
+                    <h1>Country</h1>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    <v-select solo dense  label="Select your country">
+                    </v-select>
+                </v-list-item-subtitle>
+            </v-list-content>
+
+             <v-list-content>
+                <v-list-item-title>
+                    <h1>State</h1>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    <v-select solo dense  label="Select your State">
+                    </v-select>
+                </v-list-item-subtitle>
+            </v-list-content>
+
+             <v-list-content>
+                <v-list-item-title>
+                    <h1>City</h1>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    <v-select solo dense  label="Select your City">
+                    </v-select>
+                </v-list-item-subtitle>
+            </v-list-content>
+            
+             <v-list-content>
+                <v-list-item-title>
+                    <h1>Area</h1>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    <v-select solo dense  label="Select your Area">
+                    </v-select>
+                </v-list-item-subtitle>
+            </v-list-content>
+
+             <v-list-content>
+                <v-list-item-title>
+                    <h1>Blood group</h1>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                    <v-select solo dense  label="Select your Blood group">
+                    </v-select>
+                </v-list-item-subtitle>
+            </v-list-content>
+        </v-list-item>
+    </v-list>
+    </v-card>
+</div>
+</template>
