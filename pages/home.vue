be49b1e0c447ec57f9b783e1a3f52e2d7e22034a
Pashupati <chypashu@gmail.com>
2022-01-10 15:26:05 +0545

    update

--- pages/home.vue
+++ pages/home.vue
@@ -11,7 +11,7 @@
       <v-row>
         <v-col cols="12" lg="8"> </v-col>
 
-        <v-col cols="12" lg="4">
+        <v-col cols="12" lg="3">
           <div>
             <UserDonarsearch />
           </div>
